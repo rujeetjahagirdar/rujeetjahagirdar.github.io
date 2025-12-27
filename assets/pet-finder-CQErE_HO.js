c26bdab772eb905d477f12f851b1afb1d0abc427
rujeetjahagirdar <r1jahagirdar@gmail.com>
2025-12-27 10:17:04 -0800

    Updates

---
+++ assets/pet-finder-CQErE_HO.js
@@ -0,0 +1,26 @@
+const e=`---
+id: 2
+title: Pet Finder App Design
+date: Dec 27, 2025
+readTime: 10 min read
+category: Design
+excerpt: A simple application to report missing pets.
+author: Rujeet J.
+---
+
+# Pet Finder: Report and Find missing pets nearby.
+
+Came across a wondeing pet? You can use this app to help your fellow neighbours find their wondering pets.
+
+## The Problem
+All of the pets like to escape and wonder around and this gets pet owner worried. So I designed this application which would allow users to report missing pets when they come across a wondering pet. So no more worries about your missing pet.
+
+## Features
+- No user registeration required
+- Upload a photo
+- Automatically captures the last seen location
+- Notify nearby users
+
+## Architecture
+![Architecture](/src/assets/blog-images/pet_finder.excalidraw.png)
+`;export{e as default};
