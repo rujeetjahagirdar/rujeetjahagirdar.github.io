04647e6d00e35c1fd1717ac4748c5d52d46250da
rujeetjahagirdar <r1jahagirdar@gmail.com>
2026-02-11 19:47:45 +0530

    Updates

---
+++ assets/pet-finder-lFfn67yc.js
@@ -0,0 +1,28 @@
+const e=`---
+id: 2
+title: Pet Finder
+description: A simple application to report missing pets.
+tech: Python, FastAPI, AWS, RabbitMQ
+year: 2025
+github: https://github.com/rujeetjahagirdar/
+---
+
+# Pet Finder: Report and Find missing pets nearby.
+
+Came across a wondeing pet? You can use this app to help your fellow neighbours find their wondering pets.
+
+## The Problem
+All of the pets like to escape and wonder around and this gets pet owner worried. So I designed this application which would allow users to report missing pets when they come across a wondering pet. So no more worries about your missing pet.
+
+## Features
+- No user registeration required
+- Upload a photo
+- Automatically captures the last seen location
+- Notify nearby users
+
+## Architecture
+![Architecture](/images/blog-images/pet_finder.excalidraw.png)
+
+## Screenshot
+![Project Home Screen](/images/project-images/petfinder_project_screenshot.png)
+`;export{e as default};
